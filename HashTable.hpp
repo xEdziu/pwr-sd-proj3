bcf2dbd06685ad6e1e5154e8f7285c428567d3cf
Adrian Goral <adrian.goral@gmail.com>
2024-05-12 09:56:44 +0200

    Refactor HashTable interface and add additional methods

--- HashTable.hpp
+++ HashTable.hpp
@@ -5,7 +5,13 @@ template <typename K, typename V>
 class HashTable {
  public:
     virtual void insert(const K& key, const V& value) = 0;
+    virtual V search(const K& key) = 0;
     virtual void remove(const K& key) = 0;
+    virtual bool exists(const K& key) = 0;
+    virtual size_t size() = 0;
+    virtual bool empty() = 0;
+    virtual void keys() = 0;
+    virtual void values() = 0;
     virtual void print() = 0;
     virtual ~HashTable() {}
 };
